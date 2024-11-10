57c43d2dc3662c374228257c7697bf7a77b79eda
Almas <almas.zhartybayev@gmail.com>
2024-11-10 20:06:50 +0500

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Almas Zhartybayev on 10.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
